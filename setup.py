d9e4bc30cbc53e1b5dcd7a3b5b5846e7aa11e1e7
JYLee <gimme1dollar@postech.ac.kr>
2024-07-13 12:03:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 if __name__ == "__main__":
     setup(
         name='bmoca',
-        version='2.0.1',
+        version='2.4.2',
         packages=find_packages(where='bmoca')
     )
